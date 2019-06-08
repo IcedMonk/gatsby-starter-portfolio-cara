5aed467b1283f3d58650ff35e548b66144c20610
Samyek S <39209724+IcedMonk@users.noreply.github.com>
2019-06-09 03:12:51 +0530

    testing for extra project wrapper

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -77,6 +77,14 @@ const Index = () => (
           >
             This project is my entry to Adobe's #ChallengeYourPerspective contest.
           </ProjectCard>
+          <ProjectsWrapper>
+          <ProjectCard
+            title="Freiheit"
+            link="https://www.behance.net/gallery/58937147/Freiheit"
+            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+          >
+            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+          </ProjectCard>
           <ProjectCard
             title="Harry Potter"
             link="https://www.behance.net/gallery/52915793/Harry-Potter"
